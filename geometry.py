da8f1c2308b06e0eeb090ad92481b9538ee9f2fc
edisonqkj <edison90110@gmail.com>
2015-09-16 17:52:22 +0800

    geometry.py: add some funcs; DataTypes.gdb: updated

--- geometry.py
+++ geometry.py
@@ -6,6 +6,7 @@ import os
 def CreatePoint(pt):
 	point=arcpy.Point(pt[0],pt[1])
 	print('Create Point:{}'.format(point))
+	return point
 
 def CreateMultiPoint(ptlist):
 	array=arcpy.Array()
@@ -22,6 +23,7 @@ def CreatePoyline(ptlist):
 		array.add(point)
 	polyline=arcpy.Polyline(array)
 	print('Create Polyline:{}'.format(polyline))
+	return polyline
 
 def CreatePolygon(ptlist,outshp):
 	array=arcpy.Array()
@@ -39,9 +41,11 @@ def GeometryObject(inshp):
 
 	g=arcpy.Geometry()# empty geometry object
 	geoobjlist=arcpy.Buffer_analysis(inshp,g,'100 meters')
+	print(type(geoobjlist)) # list
 	area=0
 	for obj in geoobjlist:
 		area+=obj.area
+		# print(obj.JSON)
 	print('{}\'s total Area= {}'.format(feature,area))
 
 def GeometryBuffer(ptlist,outshp):
@@ -54,13 +58,96 @@ def GeometryBuffer(ptlist,outshp):
 		geolist.append(ptgeo)
 		array.add(point)
 	plg=arcpy.Polygon(array)
-	arcpy.Buffer_analysis(plg,outshp,'1000 feet')
+	# print(plg.JSON)
+	# point buffer
+	arcpy.Buffer_analysis(geolist,outshp,'1000 feet')
+	# polygon buffer
+	arcpy.Buffer_analysis(plg,outshp+'plg','1000 feet')
+
+def PointFeature(ptlist,outshp):
+	# arcpy.CreateFeatureclass_management(outshp)
+	geolist=[]
+	for pt in ptlist:
+		point=CreatePoint(pt)
+		ptgeo=arcpy.PointGeometry(point)
+		geolist.append(ptgeo)
+		# print(ptgeo.JSON)
+	arcpy.CopyFeatures_management(geolist,outshp)
+
+def Part(inshp):
+	g=arcpy.Geometry()
+	geolist=arcpy.CopyFeatures_management(inshp,g)
+	# geilist --> [geometry objects]
+	print('len(geolist)={}'.format(len(geolist)))
+	i=0
+	for geo in geolist:
+		i+=1
+		arrays=geo.getPart()
+		print('len(arrays)={}'.format(len(arrays)))
+		# arrays
+		j=0
+		for array in arrays:
+			j+=1
+			# pt --> Point geomtry object
+			print('len(array)={}'.format(len(array)))
+			for k in range(array.count):
+				pt=array.getObject(k)
+				print('#{} shape #{} part #{} point: ({})'.format(i,j,k,pt))
+
+def PointToPolygonBoundary(point,polygon,polyline):
+	# link vertext to the points on the buffer boundary
+	ptlist=arcpy.CopyFeatures_management(point,arcpy.Geometry())
+	plglist=arcpy.CopyFeatures_management(polygon,arcpy.Geometry())
+	dirname,basename=os.path.split(polyline)
+	# des=arcpy.Describe(point)
+	# fc=arcpy.CreateFeatureclass_management(dirname,basename,"Polyline","","","",des.spatialReference)
+	# del des
+	# cur=arcpy.InsertCursor(polyline)
+	# each polygon concerning multiparts
+	print('Polygon Count: {}'.format(len(plglist)))
+
+	linelist=[]
+	for pt in ptlist:
+		# use getPart() method to get Point list from Geometry object
+		# to PointGeometry, getPart() method returns a Point object
+		# to Polygon, getPart() method returns an array of Array object,respectively corresponds to each part of the polygon feature
+		point=pt.getPart()
+		for plg in plglist:
+			for part in plg.getPart():
+				print('len(part)={}'.format(len(part)))
+				for plgpt in part:
+					# print(type(pt))
+					array=arcpy.Array()
+					array.add(point)
+					array.add(plgpt)
+					line=arcpy.Polyline(array)
+					linelist.append(line)
+
+					print('{} --> {}'.format(point,plgpt))
+					# arcpy.Append_management(line,polyline,'NO_TEST')
+					# print(line.getPart())
+
+					# using insertcursor cautions: spatialreference
+					# row=cur.newRow()
+					# row.setValue('Shape',line)
+					# cur.insertRow(row)
+					# del array,line
+	# del cur
+	arcpy.CopyFeatures_management(linelist,polyline)
+	del ptlist,plglist,linelist
 
 if __name__=='__main__':
-	arcpy.env.overwrite=True
+	arcpy.env.overwriteOutput=True
 	path='c:/py/database/sandiego.gdb/freeways'
 	ptlist=[[1277000,344000],[1283000,344000],[1283000,336000]]
-	outshp='c:/py/database/datatypes.gdb/bufofpoints1'
-	GeometryObject(path)
-	GeometryBuffer(ptlist,outshp)
-	CreatePolygon(ptlist,outshp+'1')
+	buf='c:/py/database/datatypes.gdb/bufofpoints'
+	bufplg=buf+'plg'
+	# GeometryObject(path)
+	# GeometryBuffer(ptlist,buf)
+	plg='c:/py/database/datatypes.gdb/plgofpoints'
+	# CreatePolygon(ptlist,plg)
+	pt='c:/py/database/datatypes.gdb/points'
+	# PointFeature(ptlist,pt)
+	line='c:/py/database/datatypes.gdb/lines'
+	# Part(bufplg)
+	PointToPolygonBoundary(pt,bufplg,line)
