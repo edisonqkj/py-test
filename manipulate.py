7883b260cb95ef8497f7891f4352c245de19a560
edisonqkj <edison90110@gmail.com>
2015-09-18 15:45:19 +0800

    manipulate.py: add excerise for select layer by location

--- manipulate.py
+++ manipulate.py
@@ -20,6 +20,35 @@ def MakeFeatureLayer(inshp):
 
 	arcpy.CopyFeatures_management(layer,'layershp')
 
+def function():
+	arcpy.env.workspace='c:/py/database/corvallis.gdb'
+	fld='PARK_NAME'
+	nameFld=arcpy.AddFieldDelimiters('Parks',fld)
+	value='Central Park'
+	sql=nameFld+" = '"+ value+"'"
+	arcpy.MakeFeatureLayer_management('Parks',value,sql)
+	count=arcpy.GetCount_management(value)
+	print(count)
+
+	fldinfo=arcpy.FieldInfo()
+	fldinfo.addField('RECCFLAG','','HIDDEN','')
+	fldinfo.addField('RECAFLAG','','HIDDEN','')
+	fldinfo.addField('METER_NUM','METERNUM','VISIBLE','')
+	arcpy.MakeFeatureLayer_management('ParkingMeters','Meters','','',fldinfo)
+	count=arcpy.GetCount_management('Meters')
+	print(count)
+
+	selectfeature=value
+	relation='WITHIN_A_DISTANCE'
+	distance='500 feet'
+	selecttype='NEW_SELECTION'
+	arcpy.SelectLayerByLocation_management('Meters',relation,selectfeature,distance,selecttype)
+	count=arcpy.GetCount_management('Meters')
+	print(count)
+	arcpy.CopyFeatures_management('Meters','centralparkmeters')
+
+
 if __name__=='__main__':
 	inshp='c:/py/database/datatypes.gdb/bufofpoints'
-	MakeFeatureLayer(inshp)
+	# MakeFeatureLayer(inshp)
+	function()
