d102598a5f2c2f544993cc96619168cabd2075ac
edisonqkj <edison90110@gmail.com>
2016-01-06 23:52:13 +0800

    handleweibo.py: extract points xy coordinates & saved into txt

--- handleweibo.py
+++ handleweibo.py
@@ -4,6 +4,7 @@
 '''
 
 import os
+import arcpy
 
 def getCityCode(code_file):
 	f=open(code_file,'r')
@@ -28,17 +29,93 @@ def getCityCode(code_file):
 	pattern=re.findall(u'<p>(\w+)</p>',content)
 	print(pattern)
 
+def isDigitOrFloat(str):
+	# int
+	if str.isdigit():
+		return True
+	else:# float
+		try: 
+			float(str)
+			return True
+		except(ValueError): 
+			return False
+
+		# if str.isalpha():# all character
+		# 	return False
+		# if str.isalnum():# character & number
+		# 	return False
+		# # if type(eval(str))==float:
+		# # 	return True
+		# parts=str.split('.')
+		# if len(parts)!=2:
+		# 	return False
+		# if parts[0].isdigit() or parts[1].isdigit():
+		# 	return True
+		# return True
 
 def handle(csv):
-	pass
+	# structure:
+	# poiid,title,address,lon,lat,city,category_name,checkin_num,photo_num
+	# some error occure in splitting string line as more comma is in title & address
+	# for the sake of creating points, here goes to extract lon & lat instead
+	# of all the attributes.
+	print(csv)
+	checkin_points=[]
+	f=open(csv,'r')
+	content=f.readlines()
+	f.close()
+	# print(content[0])
+	# print(content[1])
+	for line in content:
+		line=line.strip('\n')
+		items=line.split(',')
+		# print(items[3])
+		if isDigitOrFloat(items[3]):
+			checkin_points.append({'lon':items[3],'lat':items[4]})
+		else:
+			i=1
+			while(not isDigitOrFloat(items[3+i])):
+				i=i+1
+			checkin_points.append({'lon':items[3+i],'lat':items[4+i]})
+		# checkin_points.append({'poiid':items[0],\
+		# 					   'title':items[1],\
+		# 					   'address':items[2],\
+		# 					   'lon':items[3],\
+		# 					   'lat':items[4],\
+		# 					   'city':items[5],\
+		# 					   'category_name':items[6],\
+		# 					   'checkin_num':items[7],\
+		# 					   'photo_num':items[8]})
+	print(checkin_points[:5])
+	return checkin_points
+
+def toShapefile(records,outshp):
+	points=[]
+	for record in records:
+		print(record)
+		pt=arcpy.Point()
+		pt.X=record['lon']
+		pt.Y=record['lat']
+		point=arcpy.PointGeometry(pt)
+		points.append(point)
+	arcpy.CopyFeatures_management(points,outshp)
+
+def toText(records,outtxt):
+	with open(outtxt,'w') as write:
+		for record in records: 
+			print(record)
+			write.write(record['lon']+','+record['lat']+'\n')
+	write.close()
 
 if __name__=='__main__':
-	code_file=ur'E:/ACTC/3-工作安排/课程/博士生课程/0-自然班/weibo/city.html'
-	citycode=getCityCode(code_file)
+	code_file=ur'city.html'
+	# citycode=getCityCode(code_file)
 
-	data_path=ur'E:/ACTC/3-工作安排/课程/博士生课程/0-自然班/weibo/WeiboDataShare-master'
+	data_path=ur'weibo'
 	files=os.listdir(data_path)
 	print(files)
 	for f in files:
-		handle(f)
-	pass
+		if f[-3:]=='csv' and f=='aomen.csv':
+			records=handle(os.path.join(data_path,f))
+			# toShapefile(records,os.path.join(data_path,'points.shp'))
+			toText(records,os.path.join(data_path,'points.txt'))
